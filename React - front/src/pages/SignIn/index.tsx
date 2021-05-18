e6776d876c90d880bee03170acfcd8e54e142853
Emerson Lucena <emersonlucenaoficial@gmail.com>
2021-05-18 20:39:14 -0300

    finished login screen

--- React - front/src/pages/SignIn/index.tsx
+++ React - front/src/pages/SignIn/index.tsx
@@ -1,7 +1,10 @@
 import React from 'react';
-import { FiLogIn } from 'react-icons/fi'
+import { FiLogIn, FiMail, FiLock } from 'react-icons/fi'
 
 import logoImg from '../../assets/logo.svg';
+
+import Input from '../../components/input';
+import Button from '../../components/button';
 
 import { Container, Content, Background } from './styles'
 
@@ -12,10 +15,10 @@ const SignIn: React.FC = () => (
       <form>
         <h1>Faça seu logon</h1>
 
-        <input placeholder="E-mail" />
+        <Input icon={FiMail} name="email" placeholder="E-mail" />
 
-        <input type="password" placeholder="Senha" />
-        <button type="submit">Enviar</button>
+        <Input icon={FiLock} name="password" type="password" placeholder="Senha" />
+        <Button type="submit">Enviar</Button>
         <a href="forgot">Esqueci minha senha</a>
       </form>
 
--- React - front/src/pages/SignIn/styles.ts
+++ React - front/src/pages/SignIn/styles.ts
@@ -1,7 +1,7 @@
 import styled from 'styled-components';
 import { shade } from 'polished';
 
-import signInBackgroundImage from '../../assets/sign-in-background.jpg';
+import signInBackgroundImage from '../../assets/sign-in-background.png';
 
 export const Container = styled.div`
   height: 100vh; //forçando que o container tenha toda a ocupação da view height
@@ -25,34 +25,6 @@ export const Content = styled.div`
   h1 {
     margin-bottom: 24px;
   }
-  input {
-    background: #232129;
-    border-radius: 10px;
-    border: 2px solid #232129;
-    padding: 16px;
-    width: 100%;
-    color: #f4ede8;
-
-    & + input {
-      margin-top: 8px;
-    }
-  }
-  button {
-    background: #ff9000;
-    height: 56px;
-    border-radius: 10px;
-    border: 0;
-    padding: 0 16px;
-    color: #312e38;
-    width: 100%;
-    font-weight: 500;
-    margin-top: 16px;
-    transition: background-color 0.2s;
-
-    &:hover {
-      background: ${shade(0.2, '#ff9000')}
-    }
-  }
 
   a {
     color: #f4ede8;
@@ -72,10 +44,6 @@ export const Content = styled.div`
     text-decoration: none;
     display: flex;
     align-items: center;
-
-    &::placeholder {
-      color: #666360;
-    }
 
     &:hover {
       color: ${shade(0.2, '#ff9000')}
